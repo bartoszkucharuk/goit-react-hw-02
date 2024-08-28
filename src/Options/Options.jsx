143ca25451516f49225fc8a0923c980de833edc6
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-08-28 21:32:47 +0200

    Update Options.jsx

--- src/Options/Options.jsx
+++ src/Options/Options.jsx
@@ -1,31 +1,44 @@
 import React, {useState } from 'react';
 
 function Options() {
+  const [values, setValues] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+	  });
+	
+	const updateGood = () => {
+			setValues({
+			...values,
+			good: values.good + 1
+		});
+	};
 
-    const [count, setCount] = useState(0);
+	const updateNeutral= () => {
+		setValues({
+			...values,
+			neutral: values.neutral + 1
+		});
+    };
+    
+    const updateBad = () => {
+			setValues({
+			...values,
+			bad: values.bad + 1
+		});
+	};
 
-    const good = () => {
-        setCount(count + 1);
-    }
-    const neutral = () => {
-        setCount(count +1);
-    }
+  return (
+    <div>
+      <p>
+        Good: {values.good}, Neutral: {values.neutral}, Bad: { values.bad}.
+      </p>
 
-    const bad = () => { 
-        setCount(count + 1);
-    }
-    const reset = () => {
-        setCount(0);
-    }
-    return (
-        <div>
-            <p>{count}</p>
-            <button onClick={good}>good</button>
-            <button onClick={neutral}>neutral</button>
-            <button onClick={bad}>bad</button>
-            <button onClick={reset}>reset state</button>
-        </div>
-    )
-}
+      <button onClick={updateGood}>Good</button>
+      <button onClick={updateNeutral}>Neutral</button>
+      <button onClick={updateBad}>Bad</button>
+    </div>
+  );
+};
  
 export default Options;
