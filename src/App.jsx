d4e12cfbf89807e423dbb3d63fae1e719878ba47
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-08-29 19:08:02 +0200

    update localStorage saved-feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Options from './Options/Options.jsx'
 import Description from "./Description/Description.jsx"
 import Feedback from "./Feedback/Feedback.jsx"
 
 function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = window.localStorage.getItem("saved-feedback");
+
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    }
+  });
+
 
   return (
     <>
